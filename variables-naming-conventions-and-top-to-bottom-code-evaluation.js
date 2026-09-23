92456acfc1cdab87a05b54c0f9d75565a3f7e73a
SharonKendagor <sharnkend@gmail.com>
2026-09-23 09:56:37 -0700

    Updates to the naming conventions

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,13 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let name = "Alice";
+let items = 5;
+let prize = 20;
+let firstPurchase = name + " bought " + items + " items for $" + prize + ".";
 
-console.log(d);
+console.log(firstPurchase);
+items= 7;
+prize=28;
+let secondPurchase = name + " bought " + items + "items for $" + prize + ".";
+console.log(secondPurchase);
